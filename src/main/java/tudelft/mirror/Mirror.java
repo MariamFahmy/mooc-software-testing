ed289e2a7dc8c72f215b70c0dce3082f1f620a04
Mariam Ahmed <51763380+MariamFahmy@users.noreply.github.com>
2024-04-22 14:44:08 -0230

    made tests using partition-testing and fixed bugs found using those tests

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -6,8 +6,13 @@ public class Mirror {
         String mirror = "";
 
         int begin = 0;
+
+        if (string == null) {
+            return mirror;
+        }
+
         int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
+        for (; begin <= string.length() - 1 && end >= 0; begin++, end--) {
             if (string.charAt(begin) == string.charAt(end)) {
                 mirror += String.valueOf(string.charAt(end));
             }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,55 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+    Mirror mirrorObject;
+
+    @BeforeEach
+    public void initialize() {
+        this.mirrorObject = new Mirror();
+    }
+
+    @Test
+    public void inputIsNull() {
+        String result = this.mirrorObject.mirrorEnds(null);
+        Assertions.assertEquals("", result);
+    }
+
+    @Test public void oddLengthPalindrome() {
+        String result = this.mirrorObject.mirrorEnds("aba");
+        Assertions.assertEquals("aba", result);
+    }
+
+    @Test
+    public void evenLengthPalindrome() {
+        String result = this.mirrorObject.mirrorEnds("abba");
+        Assertions.assertEquals("abba", result);
+    }
+
+    @Test
+    public void oddLengthInputAndNotPalindrome() {
+        String result = this.mirrorObject.mirrorEnds("abXYZba");
+        Assertions.assertEquals("ab", result);
+    }
+
+    @Test
+    public void evenLengthInputAndNotPalindrome() {
+        String result = this.mirrorObject.mirrorEnds("abca");
+        Assertions.assertEquals("a", result);
+    }
+
+    @Test
+    public void inputHasNoMirror() {
+        String result = this.mirrorObject.mirrorEnds("abcd");
+        Assertions.assertEquals("", result);
+    }
+
+    @Test
+    public void inputIsSingleLetter() {
+        String result = this.mirrorObject.mirrorEnds("z");
+        Assertions.assertEquals("z", result);
+    }
 }
